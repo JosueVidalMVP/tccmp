501e0dcabe1aec12cf8d89a4987d5420ca39f7b3
Josue Vidal <vidalmaizena@hotmail.com>
2025-02-20 17:50:52 -0300

    Create VPC

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,4 +1,5 @@
-output "public_ip" {
-  description = "IP Público da Instância EC2"
-  value       = aws_eip.ec2_eip.public_ip
+# ID da VPC Criada
+output "vpc_id" {
+  description = "ID da VPC criada"
+  value       = aws_vpc.my_vpc.id
 }
